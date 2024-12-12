e48912f62a904ffafd643b2f2afe65830ca566a9
vstbls <7550178+vstbls@users.noreply.github.com>
2024-12-13 00:18:15 +0200

    fixes visible

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', loginView, name='login'),
     path('login/', processLoginView, name='processLogin'),
+    # FIX FOR BROKEN ACCESS CONTROL: Change the following path to path('chat/', chatView, name='chat'). See views.py for details.
     path('chat/<str:username>/', chatView, name='chat'),
     path('send/', sendMessageView, name='send'),
     path('search/', searchView, name='search')
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User, Message
 import sqlite3
 
@@ -11,14 +12,42 @@ def processLoginView(request):
         user = User.objects.filter(username=request.POST.get('username')).first()
         if user is None:
             return HttpResponse('User does not exist.')
+        '''
+        FIX FOR SENSITIVE DATA EXPOSURE:
+        We first need to protect the presently stored passwords by hashing them,
+        for example by iterating over all of them:
+        
+        for u in User.objects.all():
+            u.password = make_password(u.password)
+            u.save()
+        
+        This migration only needs to be performed once by server admins. After that,
+        it's enough to change the following if-statement to:
+        
+        if checkpassword(user.password, request.POST.get('password')):
+        '''
         if user.password == request.POST.get('password'):
             request.session['username'] = user.username
+            # Fix for broken access control: Change redirect to redirect('/chat/'). Details below.
             return redirect(f'/chat/{user.username}/')
-            # This leads to broken access control! A user can view the messages of other users just by changing the URL!
-            # This can be fixed by.... doing something...
         else:
             return redirect('/')
+'''
+FIX FOR BROKEN ACCESS CONTROL:
+For this, we want to remove the possibility of viewing messages by changing the URL.
+Since the app already stores the signed in user's username in request.session on login,
+we can use that instead:
 
+def chatView(request):
+    username = request.session['username']
+    user = User.objects.get(username=username)
+    received = Message.objects.filter(receiver=user)
+    recipients = User.objects.exclude(username=username)
+    sent = Message.objects.filter(sender=user)
+    return render(request, 'pages/chat.html', {'username': username, 'received': received, 'sent': sent, 'recipients': recipients})
+    
+We also need to change URL handling in urls.py
+'''
 def chatView(request, username):
     user = User.objects.get(username=username)
     received = Message.objects.filter(receiver=user)
@@ -27,18 +56,35 @@ def chatView(request, username):
     return render(request, 'pages/chat.html', {'username': username, 'received': received, 'sent': sent, 'recipients': recipients})
 
 def sendMessageView(request):
+    '''
+    FIX FOR CSRF: Use POST requests instead of GET:
+    
+    to = request.POST.get('to')
+    [...]
+    content = request.POST.get('content')
+    
+    This way messages will no longer be able to be sent using URLs.
+    '''
     to = request.GET.get('to')
     to_user = User.objects.get(username=to)
     from_user = User.objects.get(username=request.session['username'])
     content = request.GET.get('content')
     Message.objects.create(sender=from_user, receiver=to_user, content=content)
+    # Fix for broken access control: Change redirect to redirect('/chat/'). Details above.
     return redirect(f'/chat/{request.session["username"]}/')
 
 def searchView(request):
     searchstring = request.POST.get('searchstring', '').strip()
     uid = User.objects.get(username=request.session['username']).id
+    '''
+    FIX FOR SQL INJECTION:
+    The issue is relying on string formatting for constructing the query.
+    The query can be constructed safely without string formatting using Django's raw():
+    
+    filtered = Message.objects.raw(
+        "SELECT * FROM pages_message WHERE receiver_id=%(id) and content LIKE '%%(searchstring)%'",
+        {'id': uid, 'searchstring': searchstring}
+        )
+    '''
     filtered = Message.objects.raw("SELECT * FROM pages_message WHERE receiver_id=" + str(uid) + " and content LIKE '%" + searchstring + "%'")
-    #conn = sqlite3.connect('../db.sqlite3')
-    #c = conn.cursor()
-    #filtered = c.execute("SELECT content FROM pages_message WHERE receiver_id=" + str(uid) + "and content LIKE '%" + searchstring + "%'").fetchall()
     return render(request, 'pages/search.html', {'searchstring': searchstring, 'messages': filtered})
